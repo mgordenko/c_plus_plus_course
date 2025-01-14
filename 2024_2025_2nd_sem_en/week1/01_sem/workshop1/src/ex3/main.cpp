b74bb28e33b38b0f987209cdeb925ed0605ab3ef
Mariia K <mkgordenko@gmail.com>
2025-01-15 02:33:19 +0300

    1st seminar

---
+++ 2024_2025_2nd_sem_en/week1/01_sem/workshop1/src/ex3/main.cpp
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Workshop 1/Task 3 
+ * Displaying ASCII codes of characters.  
+ *  
+ * Output the characters 'a', 'b', 'c' and their ASCII representations.  
+ *  
+ * Use std::cout to display the characters and their corresponding ASCII codes.  
+ * Format the output to show both the character and its ASCII value.  
+ *  
+ ******************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+
+
+    return 0;
+}
